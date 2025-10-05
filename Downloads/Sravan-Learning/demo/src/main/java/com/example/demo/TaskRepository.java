3a749575dcbb88c0f6ffacbdd2d049b9798de57f
gontlasravan <gontlasravan@gmail.com>
2025-10-05 09:57:57 +0530

    Delete Downloads directory
    restructuring the project

--- Downloads/Sravan-Learning/demo/src/main/java/com/example/demo/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    // JpaRepository provides all standard CRUD methods (save, findAll, findById, deleteById, etc.)
-}
