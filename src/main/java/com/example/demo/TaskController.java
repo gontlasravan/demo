678fdfae4a43f7285f44b39392806ea4146339fa
sravan <gontlasravan@gmail.com>
2025-10-05 09:55:28 +0530

    Restructure the project

---
+++ src/main/java/com/example/demo/TaskController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController // Makes the class a REST API controller
+@RequestMapping("/api/tasks") // Base URL for all endpoints in this controller
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    // CREATE (POST) - http://localhost:8080/api/tasks   : Try now
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    // READ ALL (GET) - http://localhost:8080/api/tasks
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    // READ ONE (GET) - http://localhost:8080/api/tasks/1
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskRepository.findById(id);
+    }
+
+    // DELETE (DELETE) - http://localhost:8080/api/tasks/1
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/demo/UserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//added
+@SpringBootApplication
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+}
